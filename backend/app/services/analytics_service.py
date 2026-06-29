34e0dc0a09c64434eefc5c613f8066c6a75d4f76
nishthadobhal <nishthadobhaal2005@gmail.com>
2026-06-30 01:35:33 +0530

    consistency score added

--- backend/app/services/analytics_service.py
+++ backend/app/services/analytics_service.py
@@ -2,11 +2,14 @@ from app.services.feature_builder import build_feature_vector
 from app.services.state_estimator import estimate_state
 from app.services.recommendation_service import next_best_action
 from app.services.topic_analyzer import analyze_topics
+from app.services.consistency_service import calculate_consistency
 
 def generate_student_report(events):
     features = build_feature_vector(events)
 
     topic_analysis=analyze_topics(events)
+
+    consistency = calculate_consistency(events)
 
     state = estimate_state(features)
 
@@ -15,6 +18,7 @@ def generate_student_report(events):
     return {
         "features": features,
         "topic_analysis": topic_analysis,
+        "consistency_score": consistency,
         "state": state,
         "recommendation": recommendation
     }
