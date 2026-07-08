708967bfd585c4615b1000190aa65c2a31aa5173
nishthadobhal <nishthadobhaal2005@gmail.com>
2026-07-08 20:48:32 +0530

    learning session completed

--- backend/app/services/learning_session_service.py
+++ backend/app/services/learning_session_service.py
@@ -2,6 +2,12 @@ from sqlalchemy.orm import Session
 
 from app.models.learning_session import LearningSession
 
+from app.schemas.learning_event import LearningEventCreate
+
+from app.services.learning_event_service import (
+    create_learning_event
+)
+
 
 def create_learning_session(
     db: Session,
@@ -21,6 +27,31 @@ def create_learning_session(
     db.commit()
 
     db.refresh(session)
+
+
+    if data.completed:
+
+        event = LearningEventCreate(
+
+            student_id=data.student_id,
+
+            topic="Roadmap Topic",
+
+            duration_minutes=data.duration_minutes,
+
+            quiz_score=0,
+
+            revision=False,
+
+            notes="Generated from learning session"
+
+        )
+
+
+        create_learning_event(
+            db,
+            event
+        )
 
 
     return session
